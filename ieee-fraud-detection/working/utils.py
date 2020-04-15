5249ae83228ae7f5a6dd83a1f5854a5719eedb0e
Pi <osoriopabl@gmail.com>
2020-04-15 16:23:12 -0500

    Created method read_train_transaction that performs undersampling if required

--- ieee-fraud-detection/working/utils.py
+++ ieee-fraud-detection/working/utils.py
@@ -3,22 +3,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def read_data(file = 'train_transaction', test_size = 0.2, nrows = 30000,folder_path = None):
+def read_train_transaction(nrows = 30000,folder_path = None, 
+                    undersampling = False, RandomState=10):
     '''
     Read the data files
     Input:
+        folder_path (string): 
+            Where train_transaction is located.
+        nrows (int):
+            The number of rows to output.
+        undersampling (bool):
+            True if undersampling is required
+        RandomState (int):
+            Random state to undersampling generator
+
+    Returns:
+        X (pd.DataFrame): 
+            input_features
+        y (output): 
+            Output feature
 
     
     '''
-    if not folder_path:
-        folder_path = '~/Documents/Proyects/aprendizaje-estadistico/\
-                        ieee-fraud-detection/input/'
+    def get_X_y(train_trans):
+        X = train_trans.drop('isFraud', axis=1)
+        y = train_trans['isFraud']
+        return X, y
 
-    # Train
-    train_transaction = pd.read_csv(f'{folder_path}train_transaction.csv', nrows=nrows)
-    X, y = train_transaction.drop(['isFraud', 'TransactionID'], axis=1), train_transaction['isFraud']
-    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values.ravel(), test_size=test_size)
-    return X_train, X_test, y_train, y_test
+    def undersample_keep_ind(rus, X, y):
+        X_r, y_r = rus.fit_resample(X, y)
+        X_r.index = X.index[rus.sample_indices_]
+        y_r.index = y.index[rus.sample_indices_]
+        return X_r, y_r
+
+    if not folder_path:
+        folder_path = '~/Documents/Proyects/aprendizaje-estadistico/' \
+                       + 'ieee-fraud-detection/input/'
+    file_path = folder_path + 'train_transaction.csv'
+
+    if not undersampling:
+        train_transaction = pd.read_csv( file_path, nrows=nrows, index_col=0)
+        X, y = get_X_y(train_transaction)
+        return X, y
+
+    else:
+        from imblearn.under_sampling import RandomUnderSampler
+        
+        # Data will be readed from chuncks to ensure not to overload memory 
+        # and return requested size of data
+        rus = RandomUnderSampler(random_state=RandomState)
+        chunk_size = nrows
+        train_transaction_iterator = pd.read_csv( file_path, chunksize=chunk_size, index_col=0)
+        # Acc_table to fill with data, the expected number of rows will be nrows
+        X_acc, y_acc = pd.DataFrame(), pd.Series(dtype='int64')
+        for table in train_transaction_iterator:
+            if len(X_acc)  >= nrows:
+                break
+            # get X, y
+            X, y = get_X_y(table)
+            
+            # Undersample X, y, keep indexes
+            X_resampled, y_resampled = undersample_keep_ind(rus, X, y)
+            
+            # Accumulate
+            X_acc = pd.concat([X_acc, X_resampled])
+            y_acc = pd.concat([y_acc, y_resampled])
+        # Truncate if neccesary
+        X_acc = X_acc[: nrows]
+        y_acc = y_acc[: nrows]
+        return X_acc, y_acc
+
+
+        
 
 
 
